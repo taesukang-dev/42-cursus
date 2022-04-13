d57739c0f78fd9777caa3edd0c2c8bad86fb1b7f
Taesu Kang <mtae616@naver.com>
2022-04-13 20:37:07 +0900

    working on printf

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,33 +1,108 @@
+#include "ft_printf.h"
 #include <stdio.h>
-#include <stdarg.h>
 
-int test(char *s, ...)
+int ft_strlen(const char *s)
 {
-	va_list ap;
-	int	tmp;
+	int i;
+	
+	i = 0;
+	while(s[i])
+		i++;
+	return i;
+}
 
-	va_start(ap, s);
+int printf_c(va_list ap)
+{
+	int len;
+	char arg;
+
+	len = 0;
+	arg = va_arg(ap, int);
+	len += write(1, &arg, 1);
+	return len;
+}
+
+int printf_s(va_list ap)
+{
+	int		len;
+	char	*args;
+
+	args = va_arg(ap, char *);
+	len = ft_strlen(args);
+	write(1, args, len);
+	return len;
+}
+
+int printf_per(va_list ap)
+{
+	write(1, "%", 1);
+	return 1;
+}
+
+int find_format(char const *s, va_list ap)
+{
+	int	len;
+
+	len = 0;
+	if (*s == 'c')
+		len += printf_c(ap);
+	else if (*s == 's')
+		len += printf_s(ap);
+	else if (*s == 'd')
+		len += printf_di(ap);
+	else if (*s == '%')
+		len += printf_per(ap);
+	else if(*s == 'u')
+		len += printf_u(ap);
+	else if(*s == 'p')
+		len += printf_addr(ap);
+	else if(*s == 'x')
+		len += printf_hex_lower(ap);
+	else if(*s == 'X')
+		len += printf_hex_upper(ap);
+	return len;
+}
+
+int map_str(char const *s, va_list ap)
+{
+	int len;
+
+	len = 0;
 	while(*s)
 	{
 		if (*s == '%')
 		{
-			tmp = va_arg(ap, int);
-			printf("%d", tmp);
-			s += 2;
+			s++;
+			len += find_format(s, ap);
+			s++;
 		}
 		else
 		{
-			printf("%c", *s);
+			len += write(1, s, 1);
 			s++;
 		}
 	}
-	va_end(ap);
-
-	return 0;
+	return len;
 }
+
+int ft_printf(char const *s, ...)
+{
+	va_list ap;
+	int	len;
+
+	va_start(ap, s);
+	len = map_str(s, ap);
+	va_end(ap);
+	return len;
+}
+
 
 int main()
 {
-	test("asdasdasd %d %d", 1, 2);
+	int test = 1;
+	int temp = ft_printf("asdasdasd %c %d %% %u %p %X %x \n", 'a', 33522, -1, &test, 10, 10);
+	printf("%d\n", temp);
+
 	return 0;
 }
+
---
+++ ft_printf/printf_di.c
@@ -0,0 +1,84 @@
+#include "ft_printf.h"
+#include <stdlib.h>
+
+char	*ft_strdup(const char *s1)
+{
+	char	*ptr;
+	size_t	s1_len;
+	size_t	i;
+
+	s1_len = ft_strlen(s1);
+	ptr = (char *)malloc(sizeof(char) * s1_len + 1);
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+static size_t	ft_check_len(int n)
+{
+	size_t	i;
+
+	i = 0;
+	if (n < 0)
+		i = 1;
+	if (n == -2147483648)
+		return (10);
+	else if (n < 0)
+		n *= -1;
+	while (n > 0)
+	{
+		n /= 10;
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_itoa(int n)
+{
+	char		*buf;
+	size_t		len;
+
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	else if (n == 0 || n == -0)
+		return (ft_strdup("0"));
+	len = ft_check_len(n);
+	buf = malloc(sizeof(char) * (len + 1));
+	if (!buf)
+		return (NULL);
+	if (n < 0)
+	{
+		buf[0] = '-';
+		n *= -1;
+	}
+	buf[len] = '\0';
+	len -= 1;
+	while (n > 0)
+	{
+		buf[len] = "0123456789"[n % 10];
+		n /= 10;
+		len--;
+	}
+	return (buf);
+}
+
+int printf_di(va_list ap)
+{
+	char	*temp;
+	int		len;
+	int		arg;
+
+	arg = va_arg(ap, int);
+	temp = ft_itoa(arg);
+	len = ft_strlen(temp);
+	write(1, temp, len);
+	free(temp);
+	return len;
+}
