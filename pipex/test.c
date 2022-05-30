5271f02e85e63140cef1bb01dda3669707314632
강태수 <mtae616@naver.com>
2022-05-30 17:03:56 +0900

    update pipex/child process

--- pipex/test.c
+++ pipex/test.c
@@ -16,15 +16,32 @@ void child_process(char *argv[], char *envp[], int *fd)
 	infile = open(argv[1], O_RDONLY, 0777);
 	if (infile == -1)
 		return ;
-	if (dup2(fd[1], STDOUT_FILENO) == -1)
-		return ;
-	if (dup2(infile, STDIN_FILENO) == -1)
-		return ;
+	// if (dup2(fd[1], STDOUT_FILENO) == -1)
+	// 	return ;
+	// if (dup2(infile, STDIN_FILENO) == -1)
+	// 	return ;
 	
 	temp = ft_split(argv[2], ' ');
 	// temp[0];
 	for(int i = 0; temp[i]; i++)
 		printf("%s\n", temp[i]);
+	
+	int i = 0;
+	while(ft_strncmp(envp[i], "PATH", 4))
+		i++;
+	char *path = envp[i] + 5;
+	char **pathes = ft_split(path, ':');
+	// for(int i = 0; pathes[i]; i++)
+	// 	printf("%s\n", pathes[i]);
+	i = 0;
+	char *cmd = ft_strjoin("/", temp[0]);
+	while(pathes[i])
+	{
+		char *check = ft_strjoin(pathes[i], cmd);
+		if (!access(check, X_OK))
+			execve(check, temp, NULL);
+		i++;
+	}
 	close(fd[0]);
 }
 
@@ -44,8 +61,8 @@ int main(int argc, char *argv[], char *envp[])
 		child_process(argv, envp, fd);
 	waitpid(child, NULL, 0);
 	
-	char buf[30];
-	read(fd[0], buf, 3);
-	printf("\n%s\n", buf);
+	// char buf[30];
+	// read(fd[0], buf, 3);
+	// printf("\n%s\n", buf);
 	return 0;
 }
