247f678931ee65c708cc8ed0f366411f7935d070
Taesu Kang <mtae616@naver.com>
2022-06-16 19:20:48 +0900

    working on push_swap

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -1,14 +1,17 @@
 #include "push_swap.h"
+
+// delete it later
 #include <stdio.h>
 
-void	init_doubly(t_DoublyList *dList, char *argv[], int argc)
+void	init_doubly(t_DoublyList **dList, char *argv[], int argc)
 {
 	size_t	i;
 
+	*dList = create_doubly_list();
 	i = argc - 1;
 	while(i > 0)
 	{
-		add_dl_element(dList, ft_atoi(argv[i]));
+		add_dl_element(*dList, ft_atoi(argv[i]));
 		i--;
 	}
 }
@@ -18,17 +21,13 @@ int main(int argc, char *argv[])
 	t_DoublyList	*temp;
 	t_DoublyList	*temp2;
 	t_DoublyListNode	*buf;
-	
-	temp = create_doubly_list();
-	temp2 = create_doubly_list();
 
-	init_doubly(temp, argv, argc);
-	init_doubly(temp2, argv, argc);
+	init_doubly(&temp, argv, argc);
 	// command_pab(temp, temp2, PB);
 	// command_ss(temp, temp2);
 	// command_rab(temp, PA);
 	// command_rr(temp, temp2);
-	command_rrr(temp, temp2);
+	// command_rrr(temp, temp2);
 	buf = temp->headerNode;
 	for(int i = 0; i < temp->currentElementCount; i++)
 	{
@@ -36,13 +35,13 @@ int main(int argc, char *argv[])
 		buf = buf->next;
 	}
 	printf("===============================================================\n");
-	buf = temp2->headerNode;
-	for(int i = 0; i < temp2->currentElementCount; i++)
-	{
-		printf("%d \n", buf->data);
-		buf = buf->next;
-	}
-	printf("===============================================================\n");
+	// buf = temp2->headerNode;
+	// for(int i = 0; i < temp2->currentElementCount; i++)
+	// {
+	// 	printf("%d \n", buf->data);
+	// 	buf = buf->next;
+	// }
+	// printf("===============================================================\n");
 
 	return (0);
 }
