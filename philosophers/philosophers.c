ff9d44d3c8a1dc70a5344eca402bafc562381752
Taesu Kang <mtae616@naver.com>
2022-07-11 16:35:02 +0900

    working on philo

--- philosophers/philosophers.c
+++ philosophers/philosophers.c
@@ -2,13 +2,27 @@
 #include <stdio.h>
 #include <pthread.h>
 
-void *justPrint(void *data)
+void *justPlus(void *data)
 {
 	pthread_mutex_t mutex;
 
 	pthread_mutex_init(&mutex, NULL);
 	pthread_mutex_lock(&mutex);
 	*(int *)data += 1;
+	printf("hi! %d\n", *(int *)data);
+
+	pthread_mutex_unlock(&mutex);
+	pthread_mutex_destroy(&mutex);
+	return (0);
+}
+
+void *justMinus(void *data)
+{
+	pthread_mutex_t mutex;
+
+	pthread_mutex_init(&mutex, NULL);
+	pthread_mutex_lock(&mutex);
+	*(int *)data -= 1;
 	printf("hi! %d\n", *(int *)data);
 
 	pthread_mutex_unlock(&mutex);
@@ -24,9 +38,11 @@ int main()
 	int a = 2;
 	int status;
 	
-	pthread_create(&thread, NULL, justPrint, (void *)&a);
-	pthread_create(&thread2, NULL, justPrint, (void *)&a);
+	pthread_create(&thread, NULL, justPlus, (void *)&a);
+	pthread_create(&thread2, NULL, justMinus, (void *)&a);
+	printf("%d\n", a);
 	pthread_join(thread, (void **)&status); // 반환값 O, blocking
+	
 	// pthread_detach(thread); // 반환값 X, non-blocking
 
 	return 0;
